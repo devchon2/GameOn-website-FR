694707610cff004c2ab698b6759ee3f3a30c4add
rachon34 <rachon3441@gmail.com>
2023-03-22 21:54:22 +0100

    amélioration du menu responsive (fermeture)

--- starterOnly/nav.js
+++ starterOnly/nav.js
@@ -1,19 +1,24 @@
-//Récupération de l'eventlistener pour editer le hamburger responsive
+// Récupération de l'eventlistener pour éditer le hamburger responsive
 const iconNav = document.getElementById("IconNavBar");
+const iconLink = document.getElementsByClassName("icon");
 iconNav.addEventListener("click", editNav);
+const mainNav = document.getElementsByClassName("main-navbar");
+// Récupération des éléments du DOM pour le menu responsive
+const NavBar = document.getElementById("myTopnav");
 
-
-function editNav() {
-    var NavBar = document.getElementById("myTopnav");
-    let iconNavBar = document.getElementById("IconNavBar::active");
-    if (NavBar.className === "topnav") {
-      NavBar.className += " responsive";
-      iconNavBar.style.color = "white";
-      
-    } else {
-      NavBar.className = "topnav";
-      
-    }
+// Récupération de l'événement pour fermer le menu responsive en dehors de la zone du menu
+document.addEventListener('click', (event) => {
+  if (NavBar.classList.contains('responsive') && !NavBar.contains(event.target) && !iconNav.contains(event.target)) {
+    NavBar.classList.remove('responsive');
   }
+});
 
-  
+// Fonction pour ouvrir et fermer le menu responsive
+function editNav() {
+  if (NavBar.className === "topnav") {
+    NavBar.className += " responsive";
+    //iconNav.style.color = "white";
+  } else {
+    NavBar.className = "topnav";
+  }
+}
