bbf689a3de6cea15c9ec1d162866e1c6a2e22192
rachon34 <rachon3441@gmail.com>
2023-03-29 17:07:03 +0200

    Finalisation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -145,40 +145,7 @@ function validateLastname() { // Fonction de validation du prénom
     return true;
 }
 }
-// Récupération de l'eventlistener pour éditer le hamburger responsive
-const iconNav = document.getElementById("IconNavBar");
-const iconLink = document.querySelector("a.icon");
-iconLink.addEventListener("click", editNav);
-const mainNav = document.getElementsByClassName("main-navbar");
-// Récupération des éléments du DOM pour le menu responsive
-const NavBar = document.getElementById("myTopnav");
 
-// Récupération de l'événement pour fermer le menu responsive en dehors de la zone du menu
-document.addEventListener('click', (event) => {
-  if (  NavBar.classList.contains('responsive') && 
-        !NavBar.contains(event.target) && 
-        !iconLink.contains(event.target)) {
-          NavBar.classList.remove('responsive');
-        }
-      }
-    );
-
-// Fonction pour ouvrir et fermer le menu responsive
-function editNav() {
-  if (NavBar.className === "topnav") {
-    NavBar.className += " responsive";
-  } else {
-    NavBar.className = "topnav";
-    NavBar.classList.remove('responsive')
-  }
-}
-
-//fonction qui ferme le menu responsive au dela de 800px 
-window.addEventListener('resize', () =>{
-  if (window.innerWidth > 800) {
-    NavBar.classList.remove('responsive')
-  }
-});
 
 
 
